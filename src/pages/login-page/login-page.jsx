1c5f69f7912c5e24f8cb287ed2ef1849eec97f0b
Denis Rudenko <denis.rudenko@saltpepper.ru>
2026-02-28 16:53:45 +0300

    add toggle password button

--- src/pages/login-page/login-page.jsx
+++ src/pages/login-page/login-page.jsx
@@ -1,9 +1,17 @@
 import { Container } from '@/components/layouts/container';
 import { Button } from '@/components/ui/button';
 import { Card, CardDescription, CardTitle } from '@/components/ui/card';
-import { Field, FieldError, FieldGroup, FieldLabel, FieldSet } from '@/components/ui/field';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
+} from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import { useAuth } from '@/hooks/useAuth';
+import { Eye, EyeOff } from 'lucide-react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router';
 import { toast } from 'sonner';
@@ -12,19 +20,22 @@ export const LoginPage = () => {
   const { isLoading, login } = useAuth();
   const navigate = useNavigate();
 
+  const [showPassword, setShowPassword] = useState(false);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = async data => {
+  const onSubmit = async (data) => {
     try {
       await login(data);
       navigate('/');
       toast.success('Вы вошли в аккаунт!');
     } catch (error) {
-      const message = error.response?.data?.message || 'Произошла ошибка при входе';
+      const message =
+        error.response?.data?.message || 'Произошла ошибка при входе';
       toast.error(message);
     }
   };
@@ -37,7 +48,9 @@ export const LoginPage = () => {
             <FieldSet className="w-full max-w-xs mx-auto">
               <FieldGroup className="gap-3">
                 <CardTitle>Войдите в ваш аккаунт</CardTitle>
-                <CardDescription>Укажите данные для входа в аккаунт</CardDescription>
+                <CardDescription>
+                  Укажите данные для входа в аккаунт
+                </CardDescription>
               </FieldGroup>
               <FieldGroup className="gap-3">
                 <Field data-invalid={!!errors.email}>
@@ -58,25 +71,33 @@ export const LoginPage = () => {
                 </Field>
                 <Field data-invalid={!!errors.password}>
                   <FieldLabel htmlFor="password">Пароль</FieldLabel>
-                  <Input
-                    id="password"
-                    type="password"
-                    placeholder="••••••••"
-                    {...register('password', {
-                      required: 'Пароль обязателен',
-                      minLength: {
-                        value: 8,
-                        message: 'Пароль должен содержать минимум 8 символов',
-                      },
-                    })}
+                  <div className="relative">
+                    <Input
+                      id="password"
+                      type="password"
+                      placeholder="••••••••"
+                      type={showPassword ? 'text' : 'password'}
+                    />
+                    <Button
+                      className="absolute top-0 right-0 h-full px-3 hover:bg-transparent"
+                      onClick={() => setShowPassword(!showPassword)}
+                      size="icon"
+                      type="button"
+                      variant="ghost"
+                    >
+                      {showPassword ? (
+                        <EyeOff className="h-4 w-4 text-muted-foreground" />
+                      ) : (
+                        <Eye className="h-4 w-4 text-muted-foreground" />
+                      )}
+                    </Button>
+                  </div>
+                  <FieldError
+                    errors={errors.password ? [errors.password] : []}
                   />
-                  <FieldError errors={errors.password ? [errors.password] : []} />
                 </Field>
                 <Field orientation="horizontal">
-                  <Button
-                    type="submit"
-                    disabled={isLoading}
-                  >
+                  <Button type="submit" disabled={isLoading}>
                     {isLoading ? 'Loading...' : 'Войти'}
                   </Button>
                 </Field>
